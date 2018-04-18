c7d31abb6e761d149ca5855c5651e468c0916794
hjpnam <hjpnam@gmail.com>
2018-04-18 14:13:06 -0500

    Add files via upload

---
+++ hw_2/student_code_2.h
@@ -0,0 +1,34 @@
+///////////////////////////////////////////////////////////////////////////////
+// You need to 
+//    1. Read Problem 3 in homework assignmnet #2. 
+//    2. Implement function GetStudentName.
+//    3. Implement function MatchBracket.
+//    4. Compile your code as explained in the PDF file.
+//    5. Run the executable.
+//    6. Test and debug your code.
+//    7. Submit your code ("student_code_2.h") and results ("solution_2.dat") 
+//       via Canvas.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+//required libraries
+#include <string>
+
+//you can include standard C++ libraries here
+
+
+// This function should return your name.
+// The name should match your name in Canvas
+
+void GetStudentName(std::string& your_name)
+{
+   //replace the placeholders "Firstname" and "Lastname"
+   //with you first name and last name 
+   your_name.assign("Firstname Lastname");
+}
+
+
+int MatchBracket (const std::string& str, int position)
+{   
+   return -1; /* return correct value here*/
+}
