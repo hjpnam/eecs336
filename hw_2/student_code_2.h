a20a0f7bd939df5f233857c98f2308a69f22373e
hjpnam <hjpnam@gmail.com>
2018-04-22 00:28:42 -0500

    ez

--- hw_2/student_code_2.h
+++ hw_2/student_code_2.h
@@ -13,7 +13,8 @@
 
 //required libraries
 #include <string>
-
+#include <map>
+#include <stack>
 //you can include standard C++ libraries here
 
 
@@ -22,13 +23,73 @@
 
 void GetStudentName(std::string& your_name)
 {
-   //replace the placeholders "Firstname" and "Lastname"
-   //with you first name and last name 
-   your_name.assign("Firstname Lastname");
+  //replace the placeholders "Firstname" and "Lastname"
+  //with you first name and last name 
+  your_name.assign("Peter Nam");
 }
 
 
 int MatchBracket (const std::string& str, int position)
-{   
-   return -1; /* return correct value here*/
+{ 
+  std::map <char, char> paran_dict;
+  int i;
+  paran_dict['('] = ')';
+  paran_dict['['] = ']';
+  paran_dict['{'] = '}';
+  paran_dict[')'] = '(';
+  paran_dict[']'] = '[';
+  paran_dict['}'] = '{';
+
+  char parans[6] = {'(',')','[',']','{','}'};
+  char p = str[position];
+  std::stack <char> p_stack;
+  if (std::find(std::begin(parans), std::end(parans), p) == std::end(parans)) {
+    return -1;
+  }
+  
+  p_stack.push(paran_dict[p]);  
+  
+  if (p == '(' || p == '[' || p == '{') {
+    for (i = position + 1; i<str.length() ; ++i) {
+      if(str[i] == '(' || str[i] == '{' || str[i] == '[') {
+        p_stack.push(paran_dict[str[i]]);
+      }
+      else if(str[i] == ')' || str[i] == ']' || str[i] == '}') {
+        if (str[i] == p_stack.top()){ 
+          if (p_stack.size() > 1) {
+            p_stack.pop();
+          } 
+          else {
+            return i;
+          }
+        }
+        else {
+          return -1;
+        }
+      }
+    }
+  }
+  
+  else if (p == ')' || p == ']' || p == '}') {
+    for (i = position - 1; i>-1; --i) {
+      if(str[i] == ')' || str[i] == '}' || str[i] == ']') {
+        p_stack.push(paran_dict[str[i]]);
+      }
+      else if(str[i] == '(' || str[i] == '{' || str[i] == '[') {
+        if (str[i] == p_stack.top()) {
+          if (p_stack.size() > 1) {
+            p_stack.pop();
+          }
+          else {
+            return i;
+          }
+        }
+        else {
+          return -1;
+        }
+      }
+    }
+  }
+  
+  return -1; /* return correct value here*/
 }
