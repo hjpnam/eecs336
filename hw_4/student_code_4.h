04d68b3c256c36ac5f946d42e8ea9eed38d1514f
hjpnam <hjpnam@gmail.com>
2018-05-09 19:56:52 -0500

    Add files via upload

---
+++ hw_4/student_code_4.h
@@ -0,0 +1,38 @@
+///////////////////////////////////////////////////////////////////////////////
+// You need to 
+//    1. Read Problem 3 in homework assignmnet #4. 
+//    2. Implement function GetStudentName.
+//    3. Implement function MinMatching.
+//    4. Compile your code as explained in the PDF file.
+//    5. Run the executable.
+//    6. Test and debug your code.
+//    7. Submit your code ("student_code_4.h") and results ("solution_4.dat") 
+//       via Canvas.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+//required libraries
+#include <string>
+#include <vector>
+#include <cstring>
+
+//you can include standard C++ libraries here
+
+
+// This function should return your name.
+// The name should match your name in Canvas
+
+void GetStudentName(std::string& your_name)
+{
+   //replace the placeholders "Firstname" and "Lastname"
+   //with you first name and last name 
+   your_name.assign("Firstname Lastname");
+}
+
+int MinMatching (std::vector<int> x, std::vector<int> y)
+{ 
+   
+ //  return your value;
+   return -1;
+}
+
