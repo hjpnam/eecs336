6fd0ec9ed2b72dceda05a2b3a000627efe408151
hjpnam <hjpnam@gmail.com>
2018-05-27 18:49:15 -0500

    Add files via upload

---
+++ hw_6/student_code_6.h
@@ -0,0 +1,36 @@
+///////////////////////////////////////////////////////////////////////////////
+// You need to 
+//    1. Read Problem 2 in homework assignmnet #6.
+//    2. Implement function GetStudentName.
+//    3. Implement function FindBestSchedule.
+//    4. Compile your code as explained in the PDF file.
+//    5. Run the executable.
+//    6. Test and debug your code.
+//    7. Submit your code ("student_code_6.h") and results ("solution_6.dat") 
+//       via Canvas.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+//required libraries
+#include <string>
+#include <vector> 
+
+//you can include standard C++ libraries here
+
+
+// This function should return your name.
+// The name should match your name in Canvas
+
+void GetStudentName(std::string& your_name)
+{
+   //replace the placeholders "Firstname" and "Lastname"
+   //with you first name and last name 
+   your_name.assign("Firstname Lastname");
+}
+
+//implement the following function
+int FindMaxBalancedSequence (const std::vector<int>& weights)
+{  
+
+   return -1; /* your result */
+}
